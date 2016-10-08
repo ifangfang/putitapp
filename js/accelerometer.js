ec545d184e53004f8dc43780b771fee73a7c95e2
ridloputu <ridlo.putu@gmail.com>
2016-10-08 15:42:02 +0700

    add js

---
+++ js/accelerometer.js
@@ -0,0 +1,11 @@
+var options = { frequency: 3000 };  // Update every 3 seconds
+var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+}
+function onError() {
+    alert('onError!');
+}
